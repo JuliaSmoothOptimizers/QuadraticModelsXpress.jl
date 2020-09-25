aaf49b106ad3c02d5bd2e4f26d42464bb8b3d19d
Geoffroy Leconte <gleconte50@gmail.com>
2020-09-25 10:59:02 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.10

---
+++ src/QuadraticModelsXpress.jl
@@ -0,0 +1,14 @@
+module QuadraticModelsXpress
+
+export xpress
+
+using QuadraticModels
+using Xpress
+
+function xpress(QM)
+    prob = Xpress.XpressProblem()
+    
+
+end
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using QuadraticModelsXpress
+using Test
+
+@testset "QuadraticModelsXpress.jl" begin
+    # Write your tests here.
+end
