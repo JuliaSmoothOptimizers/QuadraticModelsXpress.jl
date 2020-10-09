f6b849fa44ba581210e80e061387c858f5cb7ddb
Geoffroy Leconte <gleconte50@gmail.com>
2020-10-09 19:31:10 +0200

    fix issues, add a lp test

--- src/QuadraticModelsXpress.jl
+++ src/QuadraticModelsXpress.jl
@@ -2,14 +2,79 @@ module QuadraticModelsXpress
 
 export xpress
 
-using QuadraticModels
-using SparseArrays
+using SolverTools
+using LinearAlgebra, SparseArrays
 using Xpress
 
+const xpress_statuses = Dict(0 => :unknown,
+                             1 => :acceptable,
+                             2 => :infeasible,
+                             3 => :exception,
+                             4 => :max_eval,
+                             5 => :unbounded,
+                             6 => :exception,
+                             7 => :exception,
+                             8 => :exception)
 
-function xpress(QM)
-    Xpress.init()
-    prob = Xpress.XpressProblem()
+function sparse_csr(I, J, V, m=maximum(I), n=maximum(J))
+    csrrowptr = zeros(Int, m+1)
+    # Compute the CSR form's row counts and store them shifted forward by one in csrrowptr
+    coolen = length(I)
+    min(length(J), length(V)) >= coolen || throw(ArgumentError("J and V need length >= length(I) = $coolen"))
+    @inbounds for k in 1:coolen
+        Ik = I[k]
+        if 1 > Ik || m < Ik
+            throw(ArgumentError("row indices I[k] must satisfy 1 <= I[k] <= m"))
+        end
+        csrrowptr[Ik+1] += 1
+    end
+
+    # Compute the CSR form's rowptrs and store them shifted forward by one in csrrowptr
+    countsum = 1
+    csrrowptr[1] = 1
+    @inbounds for i in 2:(m+1)
+        overwritten = csrrowptr[i]
+        csrrowptr[i] = countsum
+        countsum += overwritten
+    end
+
+    # Counting-sort the column and nonzero values from J and V into csrcolval and csrnzval
+    # Tracking write positions in csrrowptr corrects the row pointers
+    csrcolval = zeros(Int, length(I))
+    csrnzval = zeros(length(I))
+    @inbounds for k in 1:coolen
+        Ik, Jk = I[k], J[k]
+        if 1 > Jk || n < Jk
+            throw(ArgumentError("column indices J[k] must satisfy 1 <= J[k] <= n"))
+        end
+        csrk = csrrowptr[Ik+1]
+        csrrowptr[Ik+1] = csrk + 1
+        csrcolval[csrk] = Jk
+        csrnzval[csrk] = V[k]
+    end
+    csrrowptr = csrrowptr[1:end-1]
+    return csrrowptr, csrcolval, csrnzval
+end
+
+
+function xpress(QM; method="b", kwargs...)
+
+    prob = Ref{Xpress.Lib.XPRSprob}()
+    Xpress.Lib.XPRScreateprob(prob)
+    # use kwargs change to presolve, scaling and crossover mode
+    # example: xpress(QM, presolve=0) (see doc for other options)
+    for (k, v) in kwargs
+        if k==:presolve
+            Xpress.Lib.XPRSsetintcontrol(prob.x, Xpress.Lib.XPRS_PRESOLVE, 0)  # 0 no presolve, 1=presolve
+        elseif k==:scaling
+            GRBsetintparam(env, "ScaleFlag", v) # 0 = no scaling
+        elseif k==:crossover
+            Xpress.Lib.XPRSsetintcontrol(prob.x, Xpress.Lib.XPRS_SCALING, 0)  # 0 no scaling
+        elseif k==:display
+            Xpress.Lib.XPRSsetintcontrol(prob.x, Xpress.Lib.XPRS_CROSSOVER, 0)  # 0 no crossover
+        end
+    end
+
     srowtypes = fill(Cchar('A'), QM.meta.ncon)
     if length(QM.meta.jinf) > 0
         error("infeasible bounds in A")
@@ -52,19 +117,50 @@ function xpress(QM)
 
     A = sparse(QM.data.Arows, QM.data.Acols, QM.data.Avals, QM.meta.ncon, QM.meta.nvar)
     lvar, uvar = zeros(QM.meta.nvar), zeros(QM.meta.nvar)
+
     for i=1:QM.meta.nvar
         QM.meta.lvar[i] == -Inf ? lvar[i] = Xpress.Lib.XPRS_MINUSINFINITY : lvar[i] = QM.meta.lvar[i]
         QM.meta.uvar[i] == Inf ? uvar[i] = Xpress.Lib.XPRS_PLUSINFINITY : uvar[i] = QM.meta.uvar[i]
     end
 
-    Xpress.loadlp(prob, QM.meta.name, QM.meta.nvar, QM.meta.ncon, srowtypes,
-         rhs, drange, QM.data.c, A.colptr.-1, C_NULL, A.rowval.-1, A.nzval, lvar, uvar)
-     Xpress.lpoptimize(prob)
-     # x, slack, dual, dj = Float64[], Float64[], Float64[], Float64[]
-     # Xpress.Lib.XPRSgetsol(prob, x, slack, dual, dj)
-     # if QM.meta.nnzh > 0
-end
+    if QM.meta.nnzh > 0
+        Xpress.Lib.XPRSloadqp(prob.x, QM.meta.name, QM.meta.nvar, QM.meta.ncon, srowtypes, rhs, drange,
+                              QM.data.c, convert(Array{Cint,1}, A.colptr.-1), C_NULL,
+                              convert(Array{Cint,1}, A.rowval.-1), A.nzval, lvar, uvar, QM.meta.nnzh,
+                              convert(Array{Cint,1}, QM.data.Hrows.-1), convert(Array{Cint,1}, QM.data.Hcols.-1),
+                              QM.data.Hvals)
+    else
+        Xpress.Lib.XPRSloadlp(prob.x, QM.meta.name, QM.meta.nvar, QM.meta.ncon, srowtypes, rhs, drange,
+                              QM.data.c, convert(Array{Cint,1}, A.colptr.-1), C_NULL,
+                              convert(Array{Cint,1}, A.rowval.-1), A.nzval, lvar, uvar)
+    end
+    Xpress.Lib.XPRSchgobj(prob.x, Cint(1), Cint.([-1]), [-QM.data.c0])
 
+    t = @timed begin
+        Xpress.Lib.XPRSlpoptimize(prob.x, method)
+    end
+
+    x, y, s = zeros(QM.meta.nvar), zeros(QM.meta.ncon), zeros(QM.meta.ncon), zeros(QM.meta.nvar)
+    Xpress.Lib.XPRSgetsol(prob.x, x, C_NULL, y, s)
+    baritcnt = Ref{Cint}()
+    Xpress.Lib.XPRSgetintattrib(prob.x, Xpress.Lib.XPRS_BARITER, baritcnt)
+    objval = Ref{Float64}()
+    Xpress.Lib.XPRSgetdblattrib(prob.x, Xpress.Lib.XPRS_LPOBJVAL, objval)
+    status = Ref{Cint}()
+    Xpress.Lib.XPRSgetintattrib(prob.x, Xpress.Lib.XPRS_LPSTATUS, status)
+    p_feas = Ref{Float64}() # sum
+    Xpress.Lib.XPRSgetdblattrib(prob.x, Xpress.Lib.XPRS_BARPRIMALINF, p_feas)
+    d_feas = Ref{Float64}() # sum
+    Xpress.Lib.XPRSgetdblattrib(prob.x, Xpress.Lib.XPRS_BARDUALINF, d_feas)
+    stats = GenericExecutionStats(get(xpress_statuses, status[], :unknown),
+                                  QM, solution = x,
+                                  objective = objval[],
+                                  primal_feas = p_feas[],
+                                  dual_feas = d_feas[],
+                                  iter = Int64(baritcnt[]),
+                                  multipliers = y,
+                                  elapsed_time = t[2])
+    return stats
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,20 @@
-using QuadraticModelsXpress
+using QPSReader, QuadraticModels, QuadraticModelsXpress
 using Test
 
 @testset "QuadraticModelsXpress.jl" begin
-    # Write your tests here.
+    qps1 = readqps("QAFIRO.SIF") #lower bounds
+    stats1 = xpress(QuadraticModel(qps1))
+    @test isapprox(stats1.objective, -1.59078179, atol=1e-2)
+
+    qps2 = readqps("HS21.SIF") # low/upp bounds
+    stats2 = xpress(QuadraticModel(qps2))
+    @test isapprox(stats2.objective, -9.99599999e1, atol=1e-2)
+
+    qps3 = readqps("HS52.SIF") # free bounds
+    stats3 = xpress(QuadraticModel(qps3))
+    @test isapprox(stats3.objective, 5.32664756, atol=1e-2)
+
+    qps4 = readqps("AFIRO.SIF") # test lp
+    stats4 = xpress(QuadraticModel(qps4))
+    @test isapprox(stats4.objective, -4.6475314286e2, atol=1e-2)
 end
